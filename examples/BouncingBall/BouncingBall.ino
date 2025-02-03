1eb1f3482f56602ed7ca04b819e7dd112a0de214
Nick B <nbourrereg@hotmail.com>
2025-02-02 19:08:07 -0500

    Updated the examples

--- examples/BouncingBall/BouncingBall.ino
+++ examples/BouncingBall/BouncingBall.ino
@@ -1,11 +1,10 @@
-#include <Arduino.h>
 #include "EDGE.h"
 #include "BouncingBallScene.h"
 
 #define I2C_SDA 5
 #define I2C_SCL 6
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
+#define SCREEN_WIDTH 72
+#define SCREEN_HEIGHT 40
 
 // Examples ESP32-C3 with OLED Display
 DisplayConfig config(SSD1306, I2C_SCL, I2C_SDA, U8X8_PIN_NONE, U8G2_R0, SCREEN_WIDTH, SCREEN_HEIGHT, true);
@@ -13,17 +12,17 @@ DisplayConfig config(SSD1306, I2C_SCL, I2C_SDA, U8X8_PIN_NONE, U8G2_R0, SCREEN_W
 EDGE engine(config);
 BouncingBallScene bouncingScene;
 
-Renderer& renderer = engine.getRenderer();
-
 unsigned long currentTime = 0;
 
 void printTask(ulong time);
 
 void setup() {
     Serial.begin(115200);
+    delay(100);
+
     engine.init();
     engine.setScene(&bouncingScene);
-
+        
     Serial.println("Setup completed");
 }
 
@@ -31,4 +30,18 @@ void loop() {
     currentTime = millis();
     engine.update();
     engine.draw();
+    
+    printTask(currentTime);
+}
+
+void printTask(unsigned long ct) {
+  // If serial output is not working set "USB CDC on BOOT" to "Enabled" in tools
+  static unsigned long lastTime = 0;
+  const int rate = 2000;
+  
+  if (ct - lastTime < rate) return;
+  
+  lastTime = ct;
+  
+  Serial.print(".");
 }
--- examples/FlappyBird/FlappyBird.ino
+++ examples/FlappyBird/FlappyBird.ino
@@ -2,7 +2,15 @@
 #include "EDGE.h"
 #include "FlappyBirdScene.h"
 
-EDGE engine;
+#define I2C_SDA 5
+#define I2C_SCL 6
+#define SCREEN_WIDTH 72
+#define SCREEN_HEIGHT 40
+
+// Examples ESP32-C3 with OLED Display
+DisplayConfig config(SSD1306, I2C_SCL, I2C_SDA, U8X8_PIN_NONE, U8G2_R0, SCREEN_WIDTH, SCREEN_HEIGHT, true);
+
+EDGE engine(config);
 FlappyBirdScene scene;
 
 unsigned long currentTime = 0;

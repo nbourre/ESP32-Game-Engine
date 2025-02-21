f803282565a0bcd47819e82d575caec4fc7b6972
jim <jmwanderer@gmail.com>
2025-02-20 20:05:00 -0800

    Update button functions.
    - Add a debounce delay before next read
    - Add an isButtonReleased function
    - Add a isButtonClicked function. Useful for starting the game.
    
    Signed-off-by: jim <jmwanderer@gmail.com>

--- src/InputManager.h
+++ src/InputManager.h
@@ -7,10 +7,14 @@ class InputManager {
 public:
     void init();
     void update(unsigned long dt);
-    bool isButtonPressed();
-    bool isButtonDown();
+    bool isButtonPressed();     // True on press
+    bool isButtonReleased();    // True on release
+    bool isButtonClicked();     // True on press and release 
+    bool isButtonDown();        // True while button is pressed
 
 private:
     bool buttonState;
-    bool lastButtonState;
+    bool stateChanged;          // True if state changed in update
+    uint16_t waitTime;          // Time (ms) remaining before reading buttton
+    bool clickFlag;             // Used for isButtonClicked function
 };
